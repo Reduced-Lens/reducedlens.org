b7bec446a36c693247e336352cd46af959c1c98c
cysote <cysote@gmail.com>
2024-07-11 11:17:03 -0400

    Adding news page, changing to sans serif font family

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { MyopiaExplanationRouteComponent } from './routes/myopia-explanation-rou
 import { LandingPageRouteComponent } from './routes/landing-page-route/landing-page-route.component';
 import { RlmExplanationRouteComponent } from './routes/rlm-explanation-route/rlm-explanation-route.component';
 import { BuyingGlassesRouteComponent } from './routes/buying-glasses-route/buying-glasses-route.component';
+import { NewsRouteComponent } from './routes/news-route/news-route.component';
 
 export const routes: Routes = [
     { path: "", component: LandingPageRouteComponent, title: "Landing Page" },
+    { path: "news", component: NewsRouteComponent, title: "News" },
     { path: "myopia-explanation", component: MyopiaExplanationRouteComponent, title: "Myopia Explanation" },
     { path: "rlm-explanation", component: RlmExplanationRouteComponent, title: "Rlm Explanation" },
     { path: "buying-glasses", component: BuyingGlassesRouteComponent, title: "Buying Glasses" },
